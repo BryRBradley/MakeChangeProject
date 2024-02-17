22f9df5dcfeac541f2ec94a7e611351740a864bc
Bryan Bradley <bryrbradley@gmail.com>
2024-02-16 22:13:19 -0700

    fixed loop and break, added twenties, fifties and hundreds and refactored

--- src/com/skilldistilery/makechange/CashRegister.java
+++ src/com/skilldistilery/makechange/CashRegister.java
@@ -7,12 +7,16 @@ public class CashRegister {
 
 		double price = 0;
 		double cash = 0;
-		System.out.println("Would you like to use the cash register?");
-		System.out.println();
-		System.out.println("Please enter Y/N to continue: ");
+
 		Scanner kb = new Scanner(System.in);
-		String yN = kb.nextLine(); 
+
 		do {
+			System.out.println("Would you like to make a transaction? Enter Y/N ");
+			String yN = kb.next();
+			if (yN.equals("N") || yN.equals("n")) {
+				System.out.println("Have a nice day");
+				break;
+			}
 			System.out.println("Cash_POS is ready for transactions");
 			System.out.println();
 			System.out.println("Please enter item price with a decimal: ");
@@ -21,19 +25,34 @@ public class CashRegister {
 			System.out.println("Please enter the amount of cash tenderd: ");
 			cash = kb.nextDouble();
 			System.out.println("The price is: " + price + "  Cash tendered is: " + cash);
+
 			if (cash == price) {
 				System.out.println("That is the exact amount, No change");
-				continue;
-			} else if (cash < price) {
-				System.out.println("Not enough cash tendered");
-				continue;
-			}
-				else if(cash > price) {
 
-			
+			}
+
+			else if (cash < price) {
+				System.out.println("Not enough cash tendered");
+
+			}
+
+			else if (cash > price) {
+
 				double change = cash - price;
 
-				double changeDue = (double) (change * 100);
+				double changeDue = change * 100;
+
+				double hundreds = changeDue / 10000;
+				changeDue = changeDue % 10000;
+
+				double fifties = changeDue / 5000;
+				changeDue = changeDue % 5000;
+
+				double twenties = changeDue / 2000;
+				changeDue = changeDue % 2000;
+
+				double tens = changeDue / 1000;
+				changeDue = changeDue % 1000;
 
 				double dollars = changeDue / 100;
 				changeDue = changeDue % 100;
@@ -48,16 +67,40 @@ public class CashRegister {
 				changeDue = changeDue % 5;
 
 				double pennies = changeDue;
-				System.out.println("Give the customer: ");
-				System.out.println(Math.floor(dollars) + " Dollars");
-				System.out.println(Math.floor(quarters) + " Quarters");
-				System.out.println(Math.floor(dimes) + " Dimes");
-				System.out.println(Math.floor(nickles) + " Nickles");
-				System.out.println(Math.round(pennies) + " Pennies");
-				continue;
-			} 
-			}while(yN.equals("y"));
-			kb.close();
-		
+
+				System.out.println("The change is : " + change);
+
+				if (hundreds >= 1) {
+					System.out.println(Math.floor(hundreds) + "Hundreds");
+				}
+				if (fifties >= 1) {
+					System.out.println(Math.floor(fifties) + "Fifties");
+				}
+				if (twenties >= 1) {
+					System.out.println(Math.floor(twenties) + " Twenties");
+				}
+
+				if (tens >= 1) {
+					System.out.println(Math.round(tens) + " Tens");
+				}
+				if (dollars <= 9 && dollars > 1) {
+					System.out.println(Math.round(dollars) + " Dollars");
+				}
+				if (quarters >= 1) {
+					System.out.println(Math.round(quarters) + " Quarters");
+				}
+
+				if (dimes >= 1) {
+					System.out.println(Math.floor(dimes) + " Dimes");
+				}
+				if (nickles >= 1)
+					System.out.println(Math.floor(nickles) + " Nickles");
+
+				if (pennies >= 1)
+					System.out.println(Math.round(pennies) + " Pennies");
+
 			}
-		}
+		} while (true);
+
+	}
+}
